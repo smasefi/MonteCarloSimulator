f511152abaa93856eda052794f0a38c55f284016
Samantha Asefi <samanthamilan@gmail.com>
2025-04-29 13:27:52 -0400

    Renaming

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='MonteCarloSimulator',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/smasefi/MonteCarloSimulator',
+    author='Samantha Asefi',
+    author_email='sma9am@gmail.com',
+    license='MIT',
+    packages=['MonteCarloSimulator'],
+    install_requires=['numpy', 'pandas']
+)
