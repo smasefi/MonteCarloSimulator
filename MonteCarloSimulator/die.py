d91f2e8fe8fa992355768c8f422a90a5e9764b3c
Samantha Asefi <samanthamilan@gmail.com>
2025-04-29 12:53:47 -0400

    Adding files

---
+++ MonteCarloSimulator/die.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pandas as pd
+
+class Die:
+    '''
+    This class is representative of a die object. It contains 4 methods. The die method replicates 
+    the behavior of a dice.
+
+    1. __init__: This method initializes the die object with a list of faces and an optional weight.
+    2. change_weight: This method allows the user to change the weight of a specific face on the die.
+    3. roll: This method simulates rolling the die a specified number of times and returns the results.
+    4. currState: This method returns the current state of the die, including the faces and their weights.
+    '''
+
+    def __init__(self, faces, weight=None):
+        '''
+        This method initializes the die object with a list of faces and an optional weight. It replicates
+        what a die would look like by taking in a list of integers or strings and an optional weight. The weught
+        would change the likelihood that a particular face is chosen.
+
+        :param faces: list of integers or strings
+        :param weight: list of floats (optional)
+        :return: None'''
+        # check to make sure the type is a numpy array
+        if not isinstance(faces, np.ndarray):
+            raise TypeError("faces must be a numpy array")
+        
+        # check to make sure the array's data type is a string or int
+        if not (np.issubdtype(faces.dtype, np.integer) or np.issubdtype(faces.dtype, np.str_)):
+            raise TypeError("faces must be a list of integers or strings")
+        
+        # check if array values are unique
+        if len(faces) != len(np.unique(faces)):
+            raise ValueError("faces must be unique")
+        
+        # set weight to be an array of ones
+        weight = [1.0]*len(faces)
+        # save both face and weight to a private data frame
+        self.die = pd.DataFrame(
+            {
+                'weight': weight
+            }, index=faces
+        )
+
+
+    def change_weight(self, face, weight):
+        '''
+        This method allows the user to change the weight of a specific face on the die. Based on the 
+        position of the face, a user of this game is able to change or alter the specific weights.
+        :param face: integer or string
+        :param weight: float
+        :return: None
+        '''
+        # check to see if face is in the die
+        if face not in self.die.index:
+            raise IndexError("face not in die")
+        
+        # Checks to see if the weight is a valid type, i.e. if it is numeric
+        #(integer or float) or castable as numeric. If not, raises a
+        #`TypeError`.
+        #if weight != int(weight) or weight != float(weight):
+            #raise TypeError("weight must be a float or an int")
+        if not isinstance(weight, (int, float)):
+            raise TypeError("weight must be a float or an int")
+        # check to make sure weight is positive or greater than zero
+        if weight < 0:
+            raise ValueError("weight must be equal to 0 or positive")
+        
+        # TODO: need to write code to change the weight for alloted face
+        self.die.loc[face, 'weight'] = weight
+        # Updated the weight for the specified face
+
+    def roll(self, n=1):
+        '''
+        This method simulates rolling the die a specified number of times and returns the results of the roll.
+        The default number of rolls is one, so if nothing is passed in, it will roll once. 
+        :param n: integer
+        :return: list of outcomes
+        '''
+        # TODO: random sample with replacement from private data frame that applies weights
+        rolls = np.random.choice(self.die.index, size=n, p=self.die['weight']/self.die['weight'].sum())
+        # TODO: Returns list of outcomes
+        return rolls
+
+    def currState(self):
+        '''
+        This method returns the current state of the die, including the faces and their weights. It just returns a 
+        copy in the event that the user wants to change the die state. The copy is a deep copy, so it won't affect the original.
+        :param: None
+        :return: DataFrame of die state
+        '''
+        #TODO: Returns the current state of the die
+        return self.die.copy()
+    
---
+++ MonteCarloSimulator/game.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+from die import Die
+
+class Game:
+    '''
+    This class is representative of what a game of dice would consist of and has 3 methods. Takes the 
+    die objects and has the actions that would be taken by a game. This class has methods that exhibit
+    behaviors of a game. 
+
+    1. __init__: This method initializes the game object with a list of die objects.
+    2. play: This method simulates rolling the dice a specified number of times and returns the results.
+    3. show: This method returns the results of the game in either wide or narrow format depending on what
+    is being entered.
+    '''
+    def __init__(self, dice):
+        '''
+        This method initializes the game object with a list of die objects.
+        :param dice: list of Die objects
+        :return: None
+        '''
+        # each dice has a face and weight association via the die object --> this is a list of dice
+        #TODO: checks to make sure they all have the same number of faces
+
+        #TODO: checks list to make sure it contains Die objects
+        self.dice = dice
+        self.game_results = None
+
+    def play(self, num_rolls):
+        '''
+        This method takes in the number of rolls that should be played on a set of dice
+        and saves the results of the rolls in a private data frame.
+
+        :param num_rolls: integer
+        :return: None
+        '''
+        #TODO: take the integer and call the roll method
+        results = {}
+        for i, die in enumerate(self.dice):
+            # add the roll results into the results frame
+            results[i] = [die.roll()[0] for _ in range(num_rolls)]
+        # should be in wide format
+        #TODO: Save results to a private data frame
+        self.game_results = pd.DataFrame(results) 
+        self.game_results.index.name = 'roll' # make sure the index is the roll number
+        self.game_results.unstack() # ensure that it is wide format
+
+    def show(self, format='wide'):
+        '''
+        The show method returns the game results in either wide or narrow format depending on 
+        what it is passed as an argument. The default is wide.
+
+        :param format: 'wide' or 'narrow'
+        :return: DataFrame of game results
+        '''
+        if format == 'wide':
+            return self.game_results.copy()
+        elif format == 'narrow':
+            narrow = self.game_results.stack()
+            narrow.index.names = ['roll', 'die']
+            return narrow
+        else:
+            raise ValueError("format must be either wide or narrow")
